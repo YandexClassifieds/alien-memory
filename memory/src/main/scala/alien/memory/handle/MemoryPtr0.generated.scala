4e97436d1d91acb6fc2c77995f25def61c24a67b
robot-piglet <103436570+robot-piglet@users.noreply.github.com>
2024-04-25 11:22:21 +0000

    Deploying to gh-pages from @ YandexClassifieds/alien-memory@a480f13be53499ca370107f2bff74fd35c6e1986 🚀

--- memory/src/main/scala/alien/memory/handle/MemoryPtr0.generated.scala
+++
@@ -1,9 +0,0 @@
-package alien.memory.handle
-
-import alien.memory.*
-
-import scala.Specializable.AllNumeric
-
-case class MemoryPtr0[L <: Layout, @specialized(AllNumeric) T](
-    private[alien] val offset0: Long,
-)
--- memory/src/main/scala/alien/memory/layout/Value.scala
+++
@@ -1,9 +0,0 @@
-package alien.memory.layout
-
-import java.lang.foreign.MemoryLayout
-
-/** Represents a primitive value that can be extracted from memory.
- *
- * @constructor Creates a new sequence with inner layout `elemType` of size `n`
- */
-final case class Value[T](layout: MemoryLayout) extends Layout
